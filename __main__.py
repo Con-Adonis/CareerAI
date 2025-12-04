55818fafe7c157c7ec7659a52cf31193e505c50d
Connor Kavleski <conkavleski@gmail.com>
2025-12-03 20:18:51 -0500

    applicationDemo update ToDo: parse HTML substring from line string

--- __main__.py
+++ __main__.py
@@ -2,17 +2,24 @@ import os
 import sys
 from src.scraping.webdriver import webDriverManager
 import user.installerPages as installerPages
+#Demo imports
+import src.devTest.applicationDemo as applicationDemo
 
 def main():
     #welcome screen/ setup wizard, comment line below to remove initialization process
     installerPages.main()
+
+    #demo - run devTest logic
+    applicationDemo()
     
+    '''
     #establish web driver
     manager = webDriverManager()
     driver = manager.getDriver()
     
     #closing driver
     manager.closeDriver()
+    '''
 
 
 if __name__ == "__main__":
--- src/devTest/applicationDemo.py
+++ src/devTest/applicationDemo.py
@@ -1,17 +1,37 @@
 import os
 import time
-from sentence_transformers import SentenceTransformer, util
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+#from sentence_transformers import SentenceTransformer, util
+import re
 
+'''
 #configuration and constants
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 similarityScore = 0.85
 mockApplication = os.path.abspath("mock_job_app.html")
 
+
+#embeds and compares strings using above model
 def stringComparison(str1, str2):
     print(f"{str1} and {str2}: {util.pytorch_cos_sim(model.encode(str1), model.encode(str2)).item()}")
     return util.pytorch_cos_sim(model.encode(str1), model.encode(str2)).item()
+'''
 
-def runApplicationDemo():
+#Application parser, for demo purposes it is more hardcoded
+def readApplication():
+    pathname = "src/devTest/applicationExample.html"
+
+    #sanity check
+    if os.path.exists(pathname):
+        app = open(pathname, "r")
+    else:
+        print("Pathname Error")
+
+    #line by line parsing
+    for line in app:
+        if "label for" in line:
+            print(f"before: {line}")
+            #TODO: parse HTML info correctly
+            parseQuestion = re.search(line, "></")
+            print(f"After: {parseQuestion}")
+
+readApplication()
